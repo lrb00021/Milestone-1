ca91ddd1547d36f7ffea5f197f96d74b99193701
Lincoln Byrnside <lrb00021@mix.wvu.edu>
2026-03-08 16:38:44 -0400

    Update data_quality.sql

--- data_quality.sql
+++ data_quality.sql
@@ -1,3 +1,20 @@
---test
--- test
---test
+WITH table_row_counts AS (
+  -- Document row counts per table in the database
+  Select  'orders' AS table_name, COUNT(*) AS row_count from orders
+  union all
+  Select  'products' AS table_name, COUNT(*) AS row_count from products
+  union all
+  Select  'sellers' AS table_name, COUNT(*) AS row_count from sellers
+  union all
+  Select  'order_reviews' AS table_name, COUNT(*) AS row_count from order_reviews
+  union all
+  Select  'order_payments' AS table_name, COUNT(*) AS row_count from order_payments
+  union all
+  Select  'order_items' AS table_name, COUNT(*) AS row_count from order_items
+  union all
+  Select  'geolocation' AS table_name, COUNT(*) AS row_count from geolocation
+  union all
+  Select  'customers' AS table_name, COUNT(*) AS row_count from customers
+  union all
+  Select  'category_translation' AS table_name, COUNT(*) AS row_count from category_translation
+),
