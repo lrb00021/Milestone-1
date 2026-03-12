405aa3a0a59ad674820bf591d37da326f8c1143b
Lincoln Byrnside <lrb00021@mix.wvu.edu>
2026-03-12 16:53:12 -0400

    City order counts Jake Lotz

--- Part_Two_SQL_Audit_1.sql
+++ Part_Two_SQL_Audit_1.sql
@@ -1,1 +1,10 @@
-
+-- Which product categories are most popular with our top cities
+With city_order_counts AS(
+-- Step 1: Total orders per city
+select c.customer_city,
+count(o.order_id) as total_orders
+from orders o
+inner join customers as c on o.customer_id = c.customer_id
+group by c.customer_city
+order by total_orders desc
+),
