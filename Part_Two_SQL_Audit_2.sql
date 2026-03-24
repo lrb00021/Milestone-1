f9f4bed12accfbb2bb97c0d445928e7f13f615e2
Rumeysa Akcura <rumeysaakcura@gmail.com>
2026-03-24 16:36:21 -0400

    Adding CTE joining orders, customers, and revenue

--- Part_Two_SQL_Audit_2.sql
+++ Part_Two_SQL_Audit_2.sql
@@ -16,3 +16,21 @@ WITH order_revenue AS (
     FROM order_items oi
     GROUP BY oi.order_id
 ),
+delivered_orders_with_state AS (
+    -- Step 2:
+    -- Join orders with customers to get customer location (state)
+    -- Also attach the computed order-level revenue
+    -- Filter only delivered orders to reflect completed transactions
+    SELECT
+        o.order_id,
+        c.customer_id,
+        c.customer_unique_id,
+        c.customer_state,
+        orv.order_revenue
+    FROM orders o
+    JOIN customers c
+        ON o.customer_id = c.customer_id
+    JOIN order_revenue orv
+        ON o.order_id = orv.order_id
+    WHERE o.order_status = 'delivered'
+),
