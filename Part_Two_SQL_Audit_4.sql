0a2911ecf57ed6eee0158f8db7a9629a1c9ed875
Jake-Lotz-WVU <jtl00032@mix.wvu.edu>
2026-03-24 16:13:28 -0400

    Question 4 and the outputs

--- Part_Two_SQL_Audit_4.sql
+++ Part_Two_SQL_Audit_4.sql
@@ -1,1 +1,7 @@
-
+-- How do product catgeories perform in terms of orders, product variety, revenue and average item value
+SELECT
+    COALESCE(tr.product_category_name_english, pr.product_category_name) AS category_english,
+    COUNT(DISTINCT oi.order_id) AS total_orders,
+    COUNT(DISTINCT oi.product_id) AS unique_products,
+    ROUND(SUM(oi.price + oi.freight_value), 2) AS total_revenue,
+    ROUND(AVG(oi.price + oi.freight_value), 2) AS avg_order_item_value
