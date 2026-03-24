989e500ce3a93567e58d3eec7f1c01a322b29034
Lincoln Byrnside <lrb00021@mix.wvu.edu>
2026-03-24 16:46:51 -0400

    Adding 3rd SQL audit. Product categories with payment value above the overall average. Added documentation as well.

--- Part_Two_SQL_Audit_3.sql
+++ Part_Two_SQL_Audit_3.sql
@@ -1,1 +1,15 @@
-
+-- Products categories with a payment value above the overall average
+SELECT
+    p.product_category_name,
+    p.product_id,
+    o.order_id,
+    op.payment_value
+FROM products AS p
+INNER JOIN order_items AS o on o.product_id = p.product_id
+INNER JOIN order_payments AS op ON op.order_id = o.order_id
+-- Filtering payment values higher than the average payment value
+WHERE payment_value > (
+    SELECT AVG(payment_value) FROM order_payments
+)
+-- Showing the highest payment values first
+ORDER BY payment_value DESC;
