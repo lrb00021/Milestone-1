86d89e7e0a5baecc263db40d3bd81f9e0c7be9e0
Lincoln Byrnside <lrb00021@mix.wvu.edu>
2026-03-24 20:08:48 -0400

    Added category translation for readability. Mi no hablo espanol.

--- Part_Two_SQL_Audit_3.sql
+++ Part_Two_SQL_Audit_3.sql
@@ -1,12 +1,13 @@
 -- Products categories with a payment value above the overall average
 SELECT
-    p.product_category_name,
+    ct.product_category_name,
     p.product_id,
     o.order_id,
     op.payment_value
 FROM products AS p
 INNER JOIN order_items AS o on o.product_id = p.product_id
 INNER JOIN order_payments AS op ON op.order_id = o.order_id
+INNER JOIN category_translation AS ct ON ct.product_category_name = p.product_category_name
 -- Filtering payment values higher than the average payment value
 WHERE payment_value > (
     SELECT AVG(payment_value) FROM order_payments
